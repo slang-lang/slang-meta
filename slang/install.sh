2439d0d2b604c0a6967b86c98726bd620820557b
Michael Adelmann <madelmann@users.noreply.github.com>
2025-01-03 23:38:39 +0100

    bugfix: corrected slang install target

--- slang/install.sh
+++ slang/install.sh
@@ -7,18 +7,7 @@ VERBOSE="${1}"
 
 echo "Installing Slang..."
 
-# rebuild slang binaries
-pushd build 1>/dev/null
-
-    if [ "${VERBOSE}" == "-v" ]; then
-        make all 2>&1
-    else
-        make all 1>/dev/null 2>&1
-    fi
-
-popd 1>/dev/null
-
-# create backup of olf binaries
+# create backup of old binaries
 for file in ${EXECUTABLES[@]}; do
 	VERSION=`${TARGET_DIRECTORY}/${file} --version | sed -n 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p'`
 
