c9f54448f6c4c18151ae75fd29d8cdb3601cb731
Michael Adelmann <noreply@github.com>
2025-06-09 12:15:27 +0200

    added update.sh script to update slang extensions

---
+++ extensions/update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source config
+
+VERBOSE="${1}"
+
+echo "Updating Slang extensions:"
+
+for directory in */; do
+    pushd ${directory} 1>/dev/null
+
+        echo "Updating $(pwd)..."
+
+        pushd src 1>/dev/null
+
+            git fetch -p
+            git switch main
+            git pull
+
+        popd 1>/dev/null
+
+    popd 1>/dev/null
+done
