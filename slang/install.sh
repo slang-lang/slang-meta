a7002a95f80460c3fbd0a46b599ca95e3f178023
madelmann <madelmann@users.noreply.github.com>
2025-11-08 10:30:53 +0100

    re-introduce installing slang binaries into $TARGET_DIRECTORY while moving existing binaries to slang*_<version>

--- slang/install.sh
+++ slang/install.sh
@@ -8,11 +8,11 @@ VERBOSE="${1}"
 echo "Installing Slang..."
 
 # create backup of old binaries
-#for file in ${EXECUTABLES[@]}; do
-#	VERSION=$(${TARGET_DIRECTORY}/${file} --version | sed -n 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')
-#
-#	sudo mv "${TARGET_DIRECTORY}/${file}" "${TARGET_DIRECTORY}/${file}_${VERSION}"
-#done
+for file in ${EXECUTABLES[@]}; do
+	VERSION=$(${TARGET_DIRECTORY}/${file} --version | sed -n 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')
+
+	sudo mv "${TARGET_DIRECTORY}/${file}" "${TARGET_DIRECTORY}/${file}_${VERSION}"
+done
 
 # deploy binaries
 sudo cp ${BASEPATH}/slang/build/bin/slang*/*       /usr/bin/
