e8df53645137a8c31aa4fc5b4a7c15b0cd50d65b
madelmann <no-reply@github.com>
2026-01-11 23:40:01 +0100

    added SLANG_WORKSPACE_ROOT to cmake build add deploy target to Makefile

--- slang/install.sh
+++ slang/install.sh
@@ -3,12 +3,11 @@
 source config
 
 declare -a EXECUTABLES=( "slang" "slang-app" "slang-dbg" "slang-pkg" )
-VERBOSE="${1}"
 
 echo "Installing Slang..."
 
 # create backup of old binaries
-for file in ${EXECUTABLES[@]}; do
+for file in "${EXECUTABLES[@]}"; do
 	VERSION=$(${TARGET_DIRECTORY}/${file} --version | sed -n 's/.* \([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/p')
 
 	sudo mv "${TARGET_DIRECTORY}/${file}" "${TARGET_DIRECTORY}/${file}^${VERSION}"
